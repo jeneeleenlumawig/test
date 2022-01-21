557b5f7654c50e94f5cb6b8f7c8b6c3d4dc697b8
jeneeleenlumawig <97935950+jeneeleenlumawig@users.noreply.github.com>
2022-01-21 14:10:46 +0800

    test

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.staticfiles',
     'chatterbot.ext.django_chatterbot',
     'chat'
 ]
@@ -84,10 +85,10 @@ CHATTERBOT = {
 from chatterbot import ChatBot
 BOT = ChatBot(**CHATTERBOT)
 TRAINER = ChatterBotCorpusTrainer(BOT)
-#TRAINER.train(
+TRAINER.train(
 # "chatterbot.corpus.english",
-#    "dataset"
-# )
+    "dataset"
+#)
 
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
@@ -134,8 +135,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
-#STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
